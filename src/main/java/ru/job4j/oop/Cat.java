2c27402f02745ba9a53e8f3dbcc89c9c0bb5be6c
OlgaDzhumanova <dzhum-olga@yandex.ru>
2021-09-15 22:00:38 +0300

    Поля объекта [#166083]

---
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -0,0 +1,29 @@
+package ru.job4j.oop;
+
+public class Cat {
+    private String food;
+    private String name;
+
+    public void show() {
+        System.out.println(this.name + " eats a " + this.food);
+    }
+
+    public void giveNick(String nick){
+        this.name = nick;
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    public static void main(String[] args) {
+        Cat gav = new Cat();
+        gav.giveNick("Gav");
+        gav.eat("kotleta");
+        gav.show();
+        Cat black = new Cat();
+        black.giveNick("BlackCat");
+        black.eat("fish");
+        black.show();
+    }
+}
