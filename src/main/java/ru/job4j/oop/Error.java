7a52f87ec517d9e5c1414b9b753c2225f244c34d
OlgaDzhumanova <dzhum-olga@yandex.ru>
2021-09-15 22:23:20 +0300

    Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,33 @@
+package ru.job4j.oop;
+
+public class Error {
+    private boolean active;
+    private int status;
+    private String message;
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public Error(){
+    }
+
+    public void printInfo() {
+        System.out.println("Активность: " + active);
+        System.out.println("Статус: " + status);
+        System.out.println("Сообщение: " + message);
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error();
+        error.printInfo();
+        Error vkontakte = new Error(true, 56, "Привет");
+        vkontakte.printInfo();
+        Error odnoklassniki = new Error(true, 6,"В сети");
+        odnoklassniki.printInfo();
+        Error facebook = new Error(true, 1, "Дома");
+        facebook.printInfo();
+    }
+}
