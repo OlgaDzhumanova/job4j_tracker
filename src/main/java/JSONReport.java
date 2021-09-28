019db868e32cce3deb12c6c63a77c0446ffea3ac
OlgaDzhumanova <dzhum-olga@yandex.ru>
2021-09-28 13:57:31 +0300

    Переопределение [#174890]

---
+++ src/main/java/JSONReport.java
@@ -0,0 +1,6 @@
+public class JSONReport {
+    public String generate(String name, String body) {
+        return "{" + System.lineSeparator() + "name: " + name + System.lineSeparator() + "body: "
+                + body + System.lineSeparator() +"}";
+    }
+}
---
+++ src/main/java/ReportUsage.java
@@ -0,0 +1,11 @@
+public class ReportUsage {
+    public static void main(String[] args) {
+        HtmlReport report = new HtmlReport();
+        String text = report.generate("Report's name", "Report's body");
+        System.out.println(text);
+
+        JSONReport jsonReport = new JSONReport();
+        String textTwo = jsonReport.generate("name", "body");
+        System.out.println(textTwo);
+    }
+}
