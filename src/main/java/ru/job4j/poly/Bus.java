0cd650f17deb3af99ed28bad2dc86491c7fc72d4
OlgaDzhumanova <dzhum-olga@yandex.ru>
2021-10-14 09:38:15 +0300

    Интерфейс Input [#181092]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,17 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+
+    @Override
+    public void drive() {
+    }
+
+    @Override
+    public void passengers(int numberOfPas) {
+    }
+
+    @Override
+    public float refuel(int fuelQuantity) {
+        return 0;
+    }
+}
