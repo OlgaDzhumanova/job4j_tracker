449e6e91e57099333bcb6a74961f045fe2112146
OlgaDzhumanova <dzhum-olga@yandex.ru>
2021-10-14 10:13:16 +0300

    Интерфейс Input [#181092 #215841]

--- src/main/java/ru/job4j/poly/Bus.java
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -1,17 +1,21 @@
 package ru.job4j.poly;
 
 public class Bus implements Transport {
+    private String itinerary;
 
     @Override
     public void drive() {
+        System.out.println("Автобус движеться по марщруту: " + itinerary);
     }
 
     @Override
     public void passengers(int numberOfPas) {
+        System.out.println("Количество пассажиров на маршруте " + itinerary + " : " + numberOfPas);
     }
 
     @Override
     public float refuel(int fuelQuantity) {
-        return 0;
+        System.out.println("Затраченно топлива на сумму:");
+        return fuelQuantity;
     }
 }
