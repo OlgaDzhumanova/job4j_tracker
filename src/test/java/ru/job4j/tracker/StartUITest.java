124d64026fae2d42218b6bb753b016e19e405bfc
OlgaDzhumanova <dzhum-olga@yandex.ru>
2021-10-20 08:48:53 +0300

    Тесты вывода на консоль [#33585 #218052]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -92,7 +92,7 @@ public class StartUITest {
         Item one = tracker.add(new Item("test1"));
         Item two = tracker.add(new Item("test2"));
         Input in = new StubInput(
-                new String[] {"0", String.valueOf(one.getId()), "1"}
+                new String[] {"0", "1"}
         );
         UserAction[] actions = new UserAction[]{
                 new ShowAllAction(out),
@@ -105,7 +105,7 @@ public class StartUITest {
                         + "0. Show all items" + ln
                         + "1. Exit Program" + ln
                         + "=== Show all items ===" + ln
-                        + one.toString() + ln + two.toString() + ln
+                        + one + ln + two + ln
                         + "Menu." + ln
                         + "0. Show all items" + ln
                         + "1. Exit Program" + ln
@@ -183,7 +183,7 @@ public class StartUITest {
                         + "0. Find item by id" + ln
                         + "1. Exit Program" + ln
                         + "=== Find item by id ===" + ln
-                        +  one.toString() + ln
+                        +  one + ln
                         + "Menu." + ln
                         + "0. Find item by id" + ln
                         + "1. Exit Program" + ln
@@ -196,7 +196,7 @@ public class StartUITest {
         Tracker tracker = new Tracker();
         Item one = tracker.add(new Item("test1"));
         Input in = new StubInput(
-                new String[] {"0", String.valueOf(2), "1"}
+                new String[] {"0", "2", "1"}
         );
         UserAction[] actions = new UserAction[]{
                 new FindIdAction(out),
