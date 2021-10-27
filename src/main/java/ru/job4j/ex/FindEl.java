8bc072a1228ffa187c2bc14ae5135c1749758984
OlgaDzhumanova <dzhum-olga@yandex.ru>
2021-10-27 14:29:29 +0300

    Пользовательские исключения. [#219367 #221121]

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -3,21 +3,22 @@ package ru.job4j.ex;
 public class FindEl{
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
         int rsl = -1;
-        if (key == null) {
-            throw new ElementNotFoundException("key could not be null");
-        }
         for (int i = 0; i < value.length; i++) {
-            if (value[i] == key) {
+            if (value[i].equals(key)) {
                 rsl = i;
+                break;
             }
+        }
+        if (rsl == -1) {
+            throw new ElementNotFoundException("Element not found");
         }
         return rsl;
     }
 
     public static void main(String[] args) {
-        String[] result = new String[]{"One", null, "two", "tree", null};
+        String[] result = new String[]{"One", "two", "tree"};
         try {
-            indexOf(result, null);
+            indexOf(result, "");
         }catch (ElementNotFoundException e){
             e.printStackTrace();
         }
