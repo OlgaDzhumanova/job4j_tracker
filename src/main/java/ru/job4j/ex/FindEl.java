81e6b7e754fd20cab834f8d0608c244078540605
OlgaDzhumanova <dzhum-olga@yandex.ru>
2021-10-28 09:22:11 +0300

    Пользовательские исключения. [#219367 #221121]

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -1,6 +1,6 @@
 package ru.job4j.ex;
 
-public class FindEl{
+public class FindEl {
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
         int rsl = -1;
         for (int i = 0; i < value.length; i++) {
@@ -19,7 +19,7 @@ public class FindEl{
         String[] result = new String[]{"One", "two", "tree"};
         try {
             indexOf(result, "");
-        }catch (ElementNotFoundException e){
+        } catch (ElementNotFoundException e) {
             e.printStackTrace();
         }
     }
