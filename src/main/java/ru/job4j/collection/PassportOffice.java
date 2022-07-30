cdc61d0a2e286916a3e556cb574b927925cf38db
OlgaDzhumanova <dzhum-olga@yandex.ru>
2022-07-30 09:42:11 +0300

    1. Паспорт и жители [#212656 #326970]

--- src/main/java/ru/job4j/collection/PassportOffice.java
+++ src/main/java/ru/job4j/collection/PassportOffice.java
@@ -8,7 +8,7 @@ public class PassportOffice {
 
     public boolean add(Citizen citizen) {
         boolean rsl = false;
-        if (get(citizen.getPassport()) == null) {
+        if (!citizens.containsKey(citizen.getPassport())) {
             citizens.put(citizen.getPassport(), citizen);
             rsl = true;
         }
@@ -16,9 +16,6 @@ public class PassportOffice {
     }
 
     public Citizen get(String passport) {
-        if (citizens.containsKey(passport)) {
-            return citizens.get(passport);
-        }
-        return null;
+        return citizens.get(passport);
     }
 }
