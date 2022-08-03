13a4065be43b152784a496f392341a75e1fc5986
OlgaDzhumanova <dzhum-olga@yandex.ru>
2022-08-03 12:53:54 +0300

    3. Банковские переводы [#10038]

---
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -0,0 +1,62 @@
+package ru.job4j.bank;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BankService {
+    private final Map<User, List<Account>> users = new HashMap<>();
+
+    public void addUser(User user) {
+        users.putIfAbsent(user, new ArrayList<>());
+    }
+
+    public void addAccount(String passport, Account account) {
+        User user = findByPassport(passport);
+        if (user != null && !users.get(user).contains(account)) {
+                users.get(user).add(account);
+            }
+        }
+
+    public User findByPassport(String passport) {
+        User findUser = null;
+         for (User user : users.keySet()) {
+            if (user.getPassport().equals(passport)) {
+                findUser = user;
+            }
+        }
+        return findUser;
+    }
+
+    public Account findByRequisite(String passport, String requisite) {
+        User user = findByPassport(passport);
+        Account account = null;
+        if (user != null) {
+            for (Account result : users.get(user)) {
+                if (requisite.equals(result.getRequisite())) {
+                    account = result;
+                }
+            }
+        }
+        return account;
+    }
+
+    public boolean transferMoney(String srcPassport, String srcRequisite,
+                                 String destPassport, String destRequisite, double amount) {
+
+        boolean rsl = false;
+        Account src = findByRequisite(srcPassport, srcRequisite);
+        Account dest = findByRequisite(destPassport, destRequisite);
+        if (src != null && dest != null && src.getBalance() >= amount) {
+            src.setBalance(src.getBalance() - amount);
+            dest.setBalance(dest.getBalance() + amount);
+            rsl = true;
+        }
+        return rsl;
+    }
+
+    public List<Account> getAccounts(User user) {
+        return users.get(user);
+    }
+}
