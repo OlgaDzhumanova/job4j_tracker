657227c0149a657aa167ccf6c09645eac6eb29da
OlgaDzhumanova <dzhum-olga@yandex.ru>
2022-10-31 21:18:36 +0300

    Фильтрация учеников [#110058 #355743]

--- src/main/java/ru/job4j/stream/School.java
+++ src/main/java/ru/job4j/stream/School.java
@@ -6,7 +6,7 @@ import java.util.stream.Collectors;
 
 public class School {
     public List<Student> collect(List<Student> students, Predicate<Student> predict) {
-        return (List<Student>) students.stream()
+        return students.stream()
                 .filter(predict)
                 .collect(Collectors.toList());
     }
