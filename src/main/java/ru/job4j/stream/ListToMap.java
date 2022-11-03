57aceaf2b906103729044efc75f8cdefb6537057
OlgaDzhumanova <dzhum-olga@yandex.ru>
2022-11-03 10:07:29 +0300

    Преобразование List в Map. [#110226 #357177]

--- src/main/java/ru/job4j/stream/ListToMap.java
+++ src/main/java/ru/job4j/stream/ListToMap.java
@@ -10,7 +10,7 @@ public class ListToMap {
         return list.stream()
                 .collect(Collectors.toMap(
                         Student::getSurname,
-                        Student -> Student,
+                        student -> student,
                         (existing, replacement) -> existing
                 ));
     }
